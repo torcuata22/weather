108508b39a89a34769c1a5d8de69525b58c1cd21
torcuata22 <marilynjmarquez@gmail.com>
2023-05-09 09:22:30 -0500

    wr;ognegrjf

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -6,54 +6,57 @@ from django.shortcuts import render
 
 def index(request):
     #Read the api key from he file
-    API_KEY=open("API_KEY","r").read()
+    API_KEY="5a27f909e2203480598056d42ef7a401"
     #url for "NOW" info (from docs):
     current_weather_url="https://api.openweathermap.org/data/2.5/weather?q={}&appid={}" #{} is for dynamic data
     forecast_url="https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}$exclude=current,minutely,hourly,alerts&appid={}" #{} is for dynamic data
     #check kind of request
     #If it's a POST request:
-    if request.method == "POST":
-        city1 = request.POST['city1'] #mandatory
-        city2 = request.get('city2',None) #try to get it, but if it's not there, it's fine
-        weather_data1, daily_forecasts1 = fetch_weather_forecast(city1, API_KEY, current_weather_url, forecast_url)
-        if city2:
-            weather_data2, daily_forecasts2=fetch_weather_forecast(city2, API_KEY, current_weather_url, forecast_url)
-        else:
-            weather_data2, daily_forecasts2 = None
-        context = {
-            "weather_data1":weather_data1,
-            "daily_forecast1":daily_forecasts1,
-            "weather_data2":weather_data2,
-            "daily_forecast2":daily_forecasts2,
-        }
-        return render(request,"weather_app/index.html", context)
+    if request.method == 'POST':
+        city1 = request.POST['city1']
+        city2 = request.POST.get('city2', None)
 
-    #if it's a GET request:
+        weather_data1, daily_forecasts1 = fetch_weather_forecast(city1, API_KEY, current_weather_url, forecast_url)
+
+        if city2:
+            weather_data2, daily_forecasts2 = fetch_weather_forecast(city2, API_KEY, current_weather_url,
+                                                                         forecast_url)
+        else:
+            weather_data2, daily_forecasts2 = None, None
+
+        context = {
+            'weather_data1': weather_data1,
+            'daily_forecasts1': daily_forecasts1,
+            'weather_data2': weather_data2,
+            'daily_forecasts2': daily_forecasts2,
+        }
+
+        return render(request, 'weather_app/index.html', context)
     else:
-        return render(request,"weather_app/index.html")
+        return render(request, 'weather_app/index.html')
     
 
 #Not an endpoint:
 def fetch_weather_forecast(city, api_key, current_weather_url, forecast_url):
-    response = requests.get(current_weather_url.format(city,api_key)).json()
-    lat,lon = response['coord']['lat'], response['coord']['lon']
-    forecast_response = requests.get(forecast_url.format(lat,lon,api_key)).json()
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
 
     weather_data = {
-        "city":city,
-        "temperature":round(response['main']['temp']-273.15, 2),
-        "description":response["weather"][0]["description"],
-        "icon":response['weather'][0]['icon'],
+        'city': city,
+        'temperature': round(response['main']['temp'] - 273.15, 2),
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
     }
 
     daily_forecasts = []
     for daily_data in forecast_response['daily'][:5]:
         daily_forecasts.append({
-            "day":datetime.datetime.fromtimestamp(daily_data['dt']).strftime("%A"),
-            "min_temp":round(daily_data['temp']['min'] - 273.15, 2),
-            "max_temp":round(daily_data['temp']['max'] - 273.15, 2),
-            "description":daily_data['weather'][0]['description'],
-            "icon":daily_data['weather'][0]['icon'],
+            'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
+            'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
+            'max_temp': round(daily_data['temp']['max'] - 273.15, 2),
+            'description': daily_data['weather'][0]['description'],
+            'icon': daily_data['weather'][0]['icon'],
         })
 
     return weather_data, daily_forecasts
